84f6bd5c75ae67b963e0dcbd1fa0cb38115a885f
Neeraj <nxxwxj@gmail.com>
2022-01-17 23:31:10 +0530

    button component and realtime input validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,25 @@
 import { useState } from 'react'
 
 import Card from './shared/Card'
+import Button from './shared/Button'
 
 const FeedbackForm = () => {
   const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState('true')
+  const [message, setMessage] = useState('')
 
   const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true)
+      setMessage('Text must be atleast 10 characters')
+    } else {
+      setBtnDisabled(false)
+      setMessage(null)
+    }
+
     setText(e.target.value)
   }
 
@@ -21,8 +35,11 @@ const FeedbackForm = () => {
             value={text}
             onChange={handleTextChange}
           />
-          <button type="submit">Submit</button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Submit
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   )
