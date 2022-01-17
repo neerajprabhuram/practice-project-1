ee5102541f25fc3cd2f6b4213005845e12751bb1
Neeraj <nxxwxj@gmail.com>
2022-01-17 19:07:53 +0530

    added an option to delete a FeedbackItem

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types'
+import {FaTimes} from 'react-icons/fa'
 
 import Card from './shared/Card'
 
-const FeedbackItem = ({ item }) => {
-  return (
+const FeedbackItem = ({ item, handleDelete }) => {return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button className="close" onClick={()=>handleDelete(item.id)}>
+          <FaTimes color='purple' />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   )
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,27 +1,29 @@
 import PropTypes from 'prop-types'
 
-import FeedbackItem from "./FeedbackItem";
+import FeedbackItem from './FeedbackItem'
 
-const FeedbackList = ({ feedback }) => {
+const FeedbackList = ({ feedback, handleDelete }) => {
   if (!feedback || feedback.length === 0) {
-    return <p>No feedback yet</p>;
+    return <p>No feedback yet</p>
   }
 
-  return <div className="feedback-list">
-    {feedback.map((item)=>(
-      <FeedbackItem key={item.id} item={item} />
-    ))}
-  </div>;
-};
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+      ))}
+    </div>
+  )
+}
 
 FeedbackList.propTypes = {
-  feedback : PropTypes.arrayOf(
+  feedback: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
       text: PropTypes.string.isRequired,
       rating: PropTypes.number.isRequired,
     })
-  )
+  ),
 }
 
-export default FeedbackList;
+export default FeedbackList
