ff952ed9562f631c0b4cf12852acea4d5cb00f0a
Neeraj <nxxwxj@gmail.com>
2022-01-17 19:30:45 +0530

    added stats functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
+import FeedbackStats from './components/FeedbackStats'
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -17,6 +18,7 @@ const App = () => {
     <>
       <Header />
       <div className="container">
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
